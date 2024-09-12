69ffa00475cc00b90ddd6955001f0f9675c1a9b4
Tiago Mansur <tiago.mansur@hotmail.com>
2024-09-12 08:46:46 -0300

    feat: inclui configuração da classe dbcontext

---
+++ src/API.Catalogo/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Catalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Catalogo.Context
+{
+  /// <summary>
+  /// Classe de contexto responsável por realizar a comunicação entre as entidades de domínio (models) e o banco de dados relacional
+  /// </summary>
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+  }
+}
