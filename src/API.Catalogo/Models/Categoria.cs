715b8bde59d39d552427750f02872dba04762bac
Tiago Mansur <tiago.mansur@hotmail.com>
2024-09-12 08:17:42 -0300

    feat: cria model de produto e categoria

---
+++ src/API.Catalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Catalogo.Models
+{
+  /// <summary>
+  /// Classe que representa o domínio de Categoria
+  /// É uma classe anêmica pois define apenas propriedades
+  /// </summary>
+  public class Categoria
+  {
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+  }
+}
