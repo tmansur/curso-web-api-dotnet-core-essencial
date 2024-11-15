a9d1b68c67530fb64342ec486142398491b7aae7
tmansur <tiago.mansur@hotmail.com>
2024-11-15 19:05:45 -0300

    Aplicando o conceito de arquitetura limpa sobre o projeto de catálogo de API.

---
+++ src/Catalogo.Application/Dtos/CategoriaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalogo.Application.Dtos
+{
+  public class CategoriaDTO
+  {
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "Informe o nome da imagem")]
+    [MinLength(5)]
+    [MaxLength(250)]
+    public string ImagemUrl { get; set; }
+  }
+}
---
+++ src/Catalogo.Application/Mappings/DomainToDtoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Catalogo.Application.Dtos;
+using Catalogo.Domain.Entities;
+
+namespace Catalogo.Application.Mappings
+{
+  public class DomainToDtoMappingProfile : Profile
+  {
+    public DomainToDtoMappingProfile()
+    {
+      CreateMap<Categoria, CategoriaDTO>().ReverseMap();
+      CreateMap<Produto, ProdutoDTO>().ReverseMap();
+    }
+  }
+}
