2477455b3c6ed01f2d134d87451c6559d6cb63f6
tmansur <tiago.mansur@hotmail.com>
2024-11-15 10:27:30 -0300

    inclui teste de unidades para endpoints get do controller de produto. obs: o correto seria utilizar mock na comunicação com o banco de dados

---
+++ src/ApiCatalogoTests/UnitTests/GetProdutoTests.cs
@@ -0,0 +1,76 @@
+﻿using API.Catalogo.Controllers;
+using API.Catalogo.DTOs;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogoTests.UnitTests
+{
+  //IClassFixture permite a configuração da classe de teste GetProdutoTests como uma instância compartilhada da classe ProdutosControllerTest
+  public class GetProdutoTests : IClassFixture<ProdutosControllerTest>
+  {
+    private readonly ProdutosController _controller;
+
+    public GetProdutoTests(ProdutosControllerTest controller)
+    {
+      _controller = new ProdutosController(controller._repository, controller._mapper);
+    }
+
+    [Fact]
+    public async Task GetProdutoById_Return_OkResult()
+    {
+      //Arrange
+      var productId = 2;
+
+      //Act
+      var data = await _controller.GetAsync(productId);
+
+      //Assert (xUnit)
+      var result = Assert.IsType<OkObjectResult>(data.Result);
+      Assert.Equal(200, result.StatusCode);
+
+      //Assert (FluentAssertions)
+      data.Result.Should().BeOfType<OkObjectResult>().
+        Which.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task GetProdutoById_Return_NotFound()
+    {
+      //Arrange
+      var productId = 999;
+
+      //Act
+      var data = await _controller.GetAsync(productId);
+
+      //Assert
+      data.Result.Should().BeOfType<NotFoundObjectResult>()
+        .Which.StatusCode.Should().Be(404);
+    }
+
+    //[Fact]
+    //public async Task GetProdutoById_Return_BadRequest()
+    //{
+    //  //Arrange
+    //  var productId = -1;
+
+    //  //Act
+    //  var data = await _controller.GetAsync(productId);
+
+    //  //Assert
+    //  data.Result.Should().BeOfType<BadRequestObjectResult>()
+    //    .Which.StatusCode.Should().Be(400);
+    //}
+
+    [Fact]
+    public async Task GetProdutos_Return_ListOfProdutoDto()
+    {
+      //Act
+      var data = await _controller.GetAsync();
+
+      //Assert
+      data.Result.Should().BeOfType<OkObjectResult>()
+        .Which.Value.Should().BeAssignableTo<IEnumerable<ProdutoDto>>()
+        .And.NotBeNull();
+    }
+  }
+}
---
+++ src/ApiCatalogoTests/UnitTests/ProdutosControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using API.Catalogo.Context;
+using API.Catalogo.DTOs.Mappings;
+using API.Catalogo.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogoTests.UnitTests
+{
+  public class ProdutosControllerTest
+  {
+    public IUnitOfWork _repository;
+    public IMapper _mapper;
+
+    public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+    public static string connectionString = "Server=localhost;DataBase=apicatalogodb;Uid=root;Pwd=root";   
+
+    /// <summary>
+    /// Construtor estático que será executado apenas uma vez quando a classe for carregada pela primeira vez
+    /// Apesar do teste utilizar a infra para execução, o mais comum é mockar o acesso a infra
+    /// </summary>
+    static ProdutosControllerTest()
+    {
+      dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+        .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+        .Options;
+    }
+
+    public ProdutosControllerTest()
+    {
+      var config = new MapperConfiguration(configure =>
+      {
+        configure.AddProfile(new ProdutoDtoMappingProfile());
+      });
+
+      _mapper = config.CreateMapper();
+      var context = new AppDbContext(dbContextOptions);
+      _repository = new UnitOfWork(context);
+    }
+  }
+}
